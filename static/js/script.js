010f4da3f6a884c744edc92069aa88044ead5e3c
Richard <szaborichard7@gmail.com>
2021-07-17 11:31:19 +0200

    sorrend változtatás

--- static/js/script.js
+++ static/js/script.js
@@ -17,106 +17,46 @@ function akcio() {
     })
 }
 
-function arNovekvo() {
-    let termekek = document.querySelectorAll(".product")
-    let arak = []
-    let rendezett = []
-    for (let i = 0; i < termekek.length; i++) {
-        arak.push(parseInt(termekek[i].querySelector(".product-price").textContent.replace(" Ft", "").replace(".", "")))
-    }
-    arak.sort()
-    for (let j = 0; j < arak.length; j++) {
-        let ar = arak[j]
-        for (let k = 0; termekek.length; k++) {
-            let aktualisAr = parseInt(termekek[k].querySelector(".product-price").textContent.replace(" Ft", "").replace(".", ""))
-            if (ar === aktualisAr) {
-                rendezett.push(termekek[k])
-                break
-            }
-        }
-    }
-    let ures = document.querySelector(".products")
-    ures.innerHTML = ""
-    for (let l = 0; l < rendezett.length; l++) {
-        let imgsrc = rendezett[l].querySelector("img").src
-        let azonosito = rendezett[l].querySelector(".product-data").getAttribute("data-identifier")
-        let termekNev = rendezett[l].querySelector(".product-name").textContent
-        let termekAr = rendezett[l].querySelector(".product-price").textContent
-        let regiTermekAr = rendezett[l].querySelector(".product-old-price")
-        let pluszElem = `<div class="product">
-        <span class="image-container">
-            <img src="${imgsrc}">
-        </span>
-        <div class="product-data" data-identifier="${azonosito}">
-            <div class="product-name">${termekNev}</div>
-            <div class="product-price">${termekAr}</div>`
-        if (regiTermekAr !== null) {
-            pluszElem += `<div class="product-old-price">${regiTermekAr.textContent}</div>`
-        }
-        pluszElem += `</div>
-    </div>`
-        ures.innerHTML += pluszElem
-    }
+function arNovekvo(cica) {
+    console.log(cica.sort(function(a, b){return a-b}))
 }
 
-function arCsokkeno() {
-    let termekek = document.querySelectorAll(".product")
-    let arak = []
-    let rendezett = []
-    for (let i = 0; i < termekek.length; i++) {
-        arak.push(parseInt(termekek[i].querySelector(".product-price").textContent.replace(" Ft", "").replace(".", "")))
-    }
-    arak.reverse()
-    for (let j = 0; j < arak.length; j++) {
-        let ar = arak[j]
-        for (let k = 0; termekek.length; k++) {
-            let aktualisAr = parseInt(termekek[k].querySelector(".product-price").textContent.replace(" Ft", "").replace(".", ""))
-            if (ar === aktualisAr) {
-                rendezett.push(termekek[k])
-                break
-            }
-        }
-    }
-    let ures = document.querySelector(".products")
-    ures.innerHTML = ""
-    for (let l = 0; l < rendezett.length; l++) {
-        let imgsrc = rendezett[l].querySelector("img").src
-        let azonosito = rendezett[l].querySelector(".product-data").getAttribute("data-identifier")
-        let termekNev = rendezett[l].querySelector(".product-name").textContent
-        let termekAr = rendezett[l].querySelector(".product-price").textContent
-        let regiTermekAr = rendezett[l].querySelector(".product-old-price")
-        let pluszElem = `<div class="product">
-        <span class="image-container">
-            <img src="${imgsrc}">
-        </span>
-        <div class="product-data" data-identifier="${azonosito}">
-            <div class="product-name">${termekNev}</div>
-            <div class="product-price">${termekAr}</div>`
-        if (regiTermekAr !== null) {
-            pluszElem += `<div class="product-old-price">${regiTermekAr.textContent}</div>`
-        }
-        pluszElem += `</div>
-    </div>`
-        ures.innerHTML += pluszElem
-    }
+function arCsokkeno(kutya) {
+    console.log(kutya.reverse())
 }
 
-function abcNovekvo(termekek) {
+function abcNovekvo() {
+    let nevek = document.querySelectorAll(".product-name")
+    let joNevek = []
+    for (let i=0; i<nevek.length; i++){
+        let joNev = nevek[i]
+        joNevek.push(joNev)
+    }
+    console.log()
+}
+
+function abcCsokkeno() {
 
 }
 
-function abcCsokkeno(termekek) {
-
+function getJoArak(){
+    let arak = document.querySelectorAll(".product-price")
+    let joArak = []
+    for (let i=0; i<arak.length; i++){
+        let joAr = parseInt(arak[i].textContent.replace(" Ft", "").replace(".", ""))
+        joArak.push(joAr)
+    }
+    return joArak
 }
 
 function dropdown() {
     let opciok = document.querySelector(".order")
+    let tegla = getJoArak()
     opciok.addEventListener("change", function () {
-        console.log(document.querySelectorAll(".product"))
         if (opciok.value == 0) {
-            arNovekvo()
+            arNovekvo(tegla)
         } else if (opciok.value == 1) {
-            arCsokkeno()
+            arCsokkeno(tegla)
         } else if (opciok.value == 2) {
             abcNovekvo()
         } else {
